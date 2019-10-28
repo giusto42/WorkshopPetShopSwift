1be5f776e04756db50211be39ec90774ae505fc0
Giusto <sipos.eduard1@gmail.com>
2019-10-28 16:37:58 +0200

    Initial Commit

---
+++ PetShop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PetShop
+//
+//  Created by Sipos Eduard on 28/10/2019.
+//  Copyright © 2019 Sipos Eduard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
